1a1a05ffee07d8828a660607fe881cec7a4b1331
almacode34 <mazaira34@gmail.com>
2014-09-30 19:33:09 +0100

    Primer Commit

---
+++ js/app-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".flexslider").flexslider({animation:"slide",controlsContainer:".flexslider",slideshowSpeed:4e3,animationSpeed:300,randomize:!1,controlNav:!0,directionNav:!1})});
---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+	$('.flexslider').flexslider(
+		{
+			animation: "slide",
+			controlsContainer: ".flexslider",
+			slideshowSpeed: 4000,
+			animationSpeed: 300,
+			// animationLoop: false,
+			randomize: false,
+
+			controlNav: true,
+			directionNav: false,
+
+			// itemWidth: 320,
+			// itemMargin: 5,
+
+		}
+	);
+
+
+
+
+
+
+
+
+
+
+
+
+	
+});
